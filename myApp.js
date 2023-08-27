863fcb017788940ac35e4f20f9677dc5029566d7
Zach Dummerth <zdummerth@gmail.com>
2023-08-27 14:05:41 -0500

    create person model

--- myApp.js
+++ myApp.js
@@ -1,11 +1,18 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-let Person;
+const personSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String],
+});
+
+const Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
